311397bcda006711e8ab3a69f8356951086320e5
Orlando Varela <ovarela@ogangi.com>
2018-06-18 00:50:18 -0500

    Endpadding fix

--- src/app/result/result.component.ts
+++ src/app/result/result.component.ts
@@ -26,43 +26,44 @@ export class ResultComponent implements OnInit {
 
   calendarsRender(params) {
     const fromDate = Moment(params.date, 'MM-DD-YYYY');
-        const months = [];
-        const startMonth = fromDate.format('MMMM YYYY');
-        months.push(startMonth);
-        let monthCalendar: MonthCalendar = new MonthCalendar();
-        let dayCalendar: DayCalendar = new DayCalendar();
-        monthCalendar.name = startMonth;
-        let _indexWeeks = 0;
+    const months = [];
+    const startMonth = fromDate.format('MMMM YYYY');
+    months.push(startMonth);
+    let monthCalendar: MonthCalendar = new MonthCalendar();
+    let dayCalendar: DayCalendar = new DayCalendar();
+    monthCalendar.name = startMonth;
+    let _indexWeeks = 0;
+    monthCalendar.days[_indexWeeks] = new Array();
+    // Padding invalid days at the beggining of the month
+    this.initPadding(fromDate, monthCalendar);
+    for (let _i = 0; _i < params.n_days; _i++) {
+      const day = fromDate.clone();
+      day.add(_i, 'days');
+      console.log(day.date());
+      console.log(day.day());
+      console.log(day.format('MMMM YYYY'));
+      dayCalendar = new DayCalendar();
+      dayCalendar.day = day.date();
+      dayCalendar.day_of_the_week = day.day();
+      if (day.day() === 0 && _i !== 0) {
+        _indexWeeks++;
         monthCalendar.days[_indexWeeks] = new Array();
-        // Padding invalid days at the beggining of the month
-        this.initPadding(fromDate, monthCalendar);
-        for (let _i = 0; _i < params.n_days; _i++) {
-          const day = fromDate.clone();
-          day.add(_i, 'days');
-          console.log(day.date());
-          console.log(day.day());
-          console.log(day.format('MMMM YYYY'));
-          dayCalendar = new DayCalendar();
-          dayCalendar.day = day.date();
-          dayCalendar.day_of_the_week = day.day();
-          if (day.day() === 0 && _i !== 0) {
-            _indexWeeks++;
-            monthCalendar.days[_indexWeeks] = new Array();
-          }
-          if (!months.includes(day.format('MMMM YYYY'))) {
-            this.monthsToBeDisplayed.push(monthCalendar);
-            _indexWeeks = 0;
-            months.push(day.format('MMMM YYYY'));
-            monthCalendar = new MonthCalendar();
-            monthCalendar.days[_indexWeeks] = new Array();
-            monthCalendar.name = day.format('MMMM YYYY');
-
-          }
-          console.log(monthCalendar);
-          monthCalendar.days[_indexWeeks].push(dayCalendar);
-        }
-        // Padding invalid days at the end of the month
+      }
+      if (!months.includes(day.format('MMMM YYYY'))) {
         this.endPadding(_indexWeeks, dayCalendar, monthCalendar);
+        _indexWeeks = 0;
+        months.push(day.format('MMMM YYYY'));
+        monthCalendar = new MonthCalendar();
+        monthCalendar.days[_indexWeeks] = new Array();
+        monthCalendar.name = day.format('MMMM YYYY');
+        this.initPadding(day, monthCalendar);
+
+      }
+      console.log(monthCalendar);
+      monthCalendar.days[_indexWeeks].push(dayCalendar);
+    }
+    // Padding invalid days at the end of the month
+    this.endPadding(_indexWeeks, dayCalendar, monthCalendar);
   }
 
   initPadding(fromDate, monthCalendar) {
@@ -75,10 +76,12 @@ export class ResultComponent implements OnInit {
   }
 
   endPadding(_indexWeeks, dayCalendar, monthCalendar) {
-    for (let indexDay = dayCalendar.day_of_the_week; indexDay < 6; indexDay++) {
-      const dc: DayCalendar = new DayCalendar();
-      dc.day_of_the_week = indexDay;
-      monthCalendar.days[_indexWeeks].push(dc);
+    if (dayCalendar.day_of_the_week !== 0) {
+      for (let indexDay = dayCalendar.day_of_the_week; indexDay < 7; indexDay++) {
+        const dc: DayCalendar = new DayCalendar();
+        dc.day_of_the_week = indexDay;
+        monthCalendar.days[_indexWeeks].push(dc);
+      }
     }
     this.monthsToBeDisplayed.push(monthCalendar);
   }
