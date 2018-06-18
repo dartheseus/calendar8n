feba8dc78f1e53a0ebd0dcc781c22df9e5f1e38a
Orlando Varela <ovarela@ogangi.com>
2018-06-18 00:10:53 -0500

    Input data form

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,12 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { InputComponent } from './input/input.component';
 import { ResultComponent } from './result/result.component';
 import { AppRoutingModule } from './app-routing.module';
 import { NotFoundComponent } from './not-found/not-found.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 @NgModule({
   declarations: [
@@ -16,7 +17,9 @@ import { NotFoundComponent } from './not-found/not-found.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    FormsModule,
+    AppRoutingModule,
+    NgbModule.forRoot()
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/input/input.component.ts
+++ src/app/input/input.component.ts
@@ -1,5 +1,12 @@
 import { Component, OnInit } from '@angular/core';
 
+import { NgbDateStruct } from '@ng-bootstrap/ng-bootstrap';
+import { NgForm } from '@angular/forms';
+import { Router } from '@angular/router';
+
+
+const now = new Date();
+
 @Component({
   selector: 'app-input',
   templateUrl: './input.component.html',
@@ -7,9 +14,17 @@ import { Component, OnInit } from '@angular/core';
 })
 export class InputComponent implements OnInit {
 
-  constructor() { }
+  model: NgbDateStruct;
+  date: { year: number, month: number };
+
+  constructor(private router: Router) { }
 
   ngOnInit() {
   }
-
+  onSubmit(form: NgForm) {
+    console.log(form);
+    const formattedDate = form.value.date.month + '/' + form.value.date.day + '/' + form.value.date.year;
+    this.router.navigate(['/result'],
+      { queryParams: { date: formattedDate, n_days: form.value.n_days, country_code: form.value.country_code } });
+  }
 }
